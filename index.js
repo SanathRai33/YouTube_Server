8e70bc687f5262e0a6a2050181a06fa88021e2c8
SanathRai <sanathrai03@gmail.com>
2025-07-28 19:36:32 +0530

    YouTube Server Initiallization

---
+++ index.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+dotenv.config()
+const app = express()
+
+app.use(cors())
+app.use(express.json({limit: "30mb", extended: "true"}))
+app.use(express.urlencoded({limit: "30mb", extended: "true"}))
+
+app.get("/", ( req, res)=>{
+    res.send("YouTube backend working...")
+})
+
+app.use(bodyParser.json())
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on Port ${PORT}`)
+})
+
+const DBURL = process.env.DB_URL
+mongoose.connect(DBURL).then(()=>{
+    console.log("DataBase Connected Successfully")
+}).catch((error)=>{
+    console.log(`Error: ${error}`)
+})
